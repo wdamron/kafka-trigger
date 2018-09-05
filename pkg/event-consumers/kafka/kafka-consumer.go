0f229762d7b71843947b1cfdfba596e740ad63ee
West Damron <wdamron@illumina.com>
2018-09-05 13:22:01 -0700

    Forward message headers as HTTP headers

--- pkg/event-consumers/kafka/kafka-consumer.go
+++ pkg/event-consumers/kafka/kafka-consumer.go
@@ -20,6 +20,7 @@ import (
 	"math/rand"
 	"os"
 	"strconv"
+	"strings"
 	"sync"
 	"time"
 
@@ -151,6 +152,7 @@ func createPartitionConsumerProcess(
 	defer wg.Done()
 
 	headerBuffer := make([]byte, 0, 1024*32)
+	replacer := strings.NewReplacer("\r", "", "\n", "")
 
 	// Consume messages, wait for signal to stopchan to exit
 MessageLoop:
@@ -178,10 +180,18 @@ MessageLoop:
 			} else {
 				logrus.Infof("[%s/%d/%d] Received Kafka message: function=%s key=%s", consumer.Topic(), consumer.Partition(), msg.Offset, funcName, string(msg.Key))
 			}
+
 			req, err := utils.GetHTTPReq(clientset, funcName, ns, "kafkatriggers.kubeless.io", "POST", string(msg.Value))
 			if err != nil {
 				logrus.Errorf("[%s/%d/%d] Unable to elaborate request: function=%s key=%s err=%s", consumer.Topic(), consumer.Partition(), msg.Offset, funcName, string(msg.Key), err)
 				continue MessageLoop
+			}
+
+			if len(msg.Headers) != 0 {
+				reqHeader := req.Header
+				for _, hdr := range msg.Headers {
+					reqHeader.Add(string(hdr.Key), replacer.Replace(string(hdr.Value)))
+				}
 			}
 
 			//forward msg to function
